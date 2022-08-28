5756a244ffaeb03cdbdc3911843b496e9d8eda5a
Irfanwani <irfanwani347@gmail.com>
2022-08-28 09:02:27 +0530

    completed aave defi

--- defi_aave/scripts/aave_borrow.py
+++ defi_aave/scripts/aave_borrow.py
@@ -2,9 +2,11 @@ from scripts.get_weth import get_weth
 from .helpful_scripts import get_account
 from brownie import interface, config, network
 from web3 import Web3
+from solcx import install_solc
 
-amount = Web3.toWei(0.005, "ether")
+amount = Web3.toWei(0.1, "ether")
 
+install_solc('0.8.10')
 
 def main():
     account = get_account()
@@ -14,7 +16,6 @@ def main():
         get_weth()
 
     lending_pool = get_lending_pool()
-    print(lending_pool)
 
     # Approve sending out erc20 tokens
     approve_erc20(amount, lending_pool.address, erc20_address, account)
@@ -27,31 +28,33 @@ def main():
     borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
     print("lets borrow!!")
 
-    dai_eth_price_feed = config["networks"][network.show_active()]["dai_eth_price_feed"]
-    dai_eth_price = get_asset_price(dai_eth_price_feed)
+    link_eth_price_feed = config["networks"][network.show_active()][
+        "link_eth_price_feed"
+    ]
+    link_eth_price = get_asset_price(link_eth_price_feed)
 
-    amount_dai_to_borrow = (borrowable_eth * 0.75) / dai_eth_price
+    amount_link_to_borrow = (borrowable_eth * 0.95) / link_eth_price
 
     print(
-        f"Borrowing {amount_dai_to_borrow} DAI ({Web3.toWei(borrowable_eth*0.75, 'ether')} wei)"
+        f"Borrowing {amount_link_to_borrow} LINK ({Web3.toWei(borrowable_eth*0.95, 'ether')} wei)"
     )
 
-    dai_address = config["networks"][network.show_active()]["dai_token"]
+    link_address = config["networks"][network.show_active()]["link_token"]
 
     borrow_tx = lending_pool.borrow(
-        dai_address,
-        Web3.toWei(borrowable_eth * 0.75, "ether"),
+        link_address,
+        Web3.toWei(amount_link_to_borrow, "ether"),
         1,
         0,
         account.address,
-        {"from": account, 'gasPrice': 9000000},
+        {"from": account},
     )
 
     borrow_tx.wait(1)
-    print("we borrowed some DAI")
+    print("we borrowed some link")
     get_borrowable_data(lending_pool, account)
 
-    # repay_all(amount, lending_pool, account)
+    repay_all(amount, lending_pool, account)
 
     print("You just deposited , borrowed and repayed with aave, brownie and chainlink")
 
@@ -60,12 +63,12 @@ def repay_all(amount, lending_pool, account):
     approve_erc20(
         Web3.toWei(amount, "ether"),
         lending_pool,
-        config["networks"][network.show_active()]["dai_token"],
+        config["networks"][network.show_active()]["link_token"],
         account,
     )
 
     repay_tx = lending_pool.repay(
-        config["networks"][network.show_active()]["dai_token"],
+        config["networks"][network.show_active()]["link_token"],
         amount,
         1,
         account.address,
@@ -77,11 +80,11 @@ def repay_all(amount, lending_pool, account):
 
 
 def get_asset_price(price_feed_address):
-    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    link_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
 
-    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    latest_price = link_eth_price_feed.latestRoundData()[1]
     converted_lp = Web3.fromWei(latest_price, "ether")
-    print(f"Latest Dai eth price is : {converted_lp}")
+    print(f"Latest link eth price is : {converted_lp}")
     return float(converted_lp)
 
 
@@ -115,11 +118,11 @@ def approve_erc20(amount, spender, erc20_address, account):
 
 
 def get_lending_pool():
-    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+    lending_pool_addresses_provider = interface.IPoolAddressesProvider(
         config["networks"][network.show_active()]["lending_pool_addresses_provider"]
     )
 
-    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool_address = lending_pool_addresses_provider.getPool()
 
-    lending_pool = interface.ILendingPool(lending_pool_address)
+    lending_pool = interface.IPool(lending_pool_address)
     return lending_pool
